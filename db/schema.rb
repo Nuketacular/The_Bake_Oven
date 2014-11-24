1b9b3664caa9cc1e2844660ec900e7d0bb846784
Graham G.N. Jones <grahamgnjones@gmail.com>
2014-11-24 11:24:55 -0600

    Added photos to some products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124161200) do
+ActiveRecord::Schema.define(version: 20141124165903) do
 
   create_table "about_us", force: true do |t|
     t.string   "title"
