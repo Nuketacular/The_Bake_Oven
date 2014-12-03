3b09eacf7ca0661ed850f24c62443e39a6ccab2e
Graham G.N. Jones <grahamgnjones@gmail.com>
2014-12-03 11:41:30 -0600

    Added category search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,7 @@ class HomeController < ApplicationController
   def search_results
     wildcard_keywords = '%' + params[:search_keywords] + '%'
     @search_results = Product.where('name LIKE ?', wildcard_keywords)
+    @category_search_results = Product.where('name LIKE ? AND category_id = ?',
+                                             wildcard_keywords, params[:category_id])
   end
 end
